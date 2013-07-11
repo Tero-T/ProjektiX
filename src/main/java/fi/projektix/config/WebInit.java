73dc0f5ba907e6b29bcf2cc08bd87c780e49cc9b
Peter Vilja <peter.vilja@elisa.fi>
2013-07-11 19:01:00 +0300

    added configuration for spring

---
+++ src/main/java/fi/projektix/config/WebInit.java
@@ -0,0 +1,24 @@
+package fi.projektix.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebInit implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext container) {
+		System.out.println("testginss");
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(AppConfig.class);
+		
+		ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(ctx));
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
+	}
+}
